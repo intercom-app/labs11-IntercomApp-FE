7403151c4d110574c305be82575be0e7d0854d26
Sergey Osipyan <osipyansergey@gmail.com>
2019-03-20 10:40:44 -0700

    remove ios folder

---
+++ iOS/Voice Chat/Voice Chat/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Voice Chat
+//
+//  Created by Sergey Osipyan on 3/19/19.
+//  Copyright © 2019 Sergey Osipyan. All rights reserved.
+//
+
+import UIKit
+import TwilioVoice
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
