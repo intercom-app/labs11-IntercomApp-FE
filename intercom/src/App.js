d4d34e9abb0f34000b2f10a7e481df47e7420a28
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2019-03-21 08:10:26 -0700

    users component created

--- intercom/src/App.js
+++ intercom/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Link } from "react-router-dom";
 import Users from './components/Users/Users';
 import Intro from './components/Intro/Intro';
+import Register from './components/Register/Register';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="App">
         <Route exact path="/" component={Intro} />
         <Route path="/users" component={Users} />
+        <Route path="/register" component={Register} />        
       </div>
     );
   }
