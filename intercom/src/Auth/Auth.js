aa50d8e66a14224b6607a9724aa047cd8bf966a0
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-25 19:15:11 -0400

    Created Auth components. Installed required dependencies. Create auth var file based on .env file.

---
+++ intercom/src/Auth/Auth.js
@@ -0,0 +1,88 @@
+import auth0 from 'auth0-js';
+import history from '../history';
+import { AUTH_CONFIG } from './authVars';
+
+export default class Auth {
+  accessToken;
+  idToken;
+  expiresAt;
+
+  auth0 = new auth0.WebAuth({
+    domain: AUTH_CONFIG.domain,
+    clientID: AUTH_CONFIG.clientID,
+    redirectUri: AUTH_CONFIG.redirectUri,
+    audience: AUTH_CONFIG.audience,
+    responseType: 'token id_token',
+    scope: 'openid email profile'
+  });
+
+  login = () => {
+    this.auth0.authorize();
+  }
+
+  handleAuthentication = () => {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+      } else if (err) {
+        history.replace('/intro');
+        console.log(err);
+        alert(`Error: ${err.error}. Check the console for further details.`);
+      }
+    });
+  }
+
+  getAccessToken = () => {
+    return this.accessToken;
+  }
+
+  getIdToken = () => {
+    return this.idToken;
+  }
+
+  setSession = (authResult) => {
+    // Set isLoggedIn flag in localStorage
+    localStorage.setItem('isLoggedIn', 'true');
+
+    // Set the time that the access token will expire at
+    let expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
+    this.accessToken = authResult.accessToken;
+    this.idToken = authResult.idToken;
+    this.expiresAt = expiresAt;
+
+    // navigate to the home route
+    history.replace('/intro');
+  }
+
+  renewSession = () => {
+    this.auth0.checkSession({}, (err, authResult) => {
+       if (authResult && authResult.accessToken && authResult.idToken) {
+         this.setSession(authResult);
+       } else if (err) {
+         this.logout();
+         console.log(err);
+         alert(`Could not get a new token (${err.error}: ${err.error_description}).`);
+       }
+    });
+  }
+
+  logout = () => {
+    // Remove tokens and expiry time
+    this.accessToken = null;
+    this.idToken = null;
+    this.expiresAt = 0;
+
+    // Remove isLoggedIn flag from localStorage
+    localStorage.removeItem('isLoggedIn');
+
+    // navigate to the home route
+    history.replace('/intro');
+  }
+
+  isAuthenticated = () => {
+    // Check whether the current time is past the
+    // access token's expiry time
+    let expiresAt = this.expiresAt;
+    return new Date().getTime() < expiresAt;
+  }
+}
