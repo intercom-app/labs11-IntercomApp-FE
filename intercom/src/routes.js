eac6a3fd8283dbcb054970be364c4c72f47daa5c
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-25 19:10:04 -0400

    Created routes.js to src root.

---
+++ intercom/src/routes.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+
+import history from './history';
+
+// Main Router  
+import App from './App';
+
+// Auth Routes
+import Auth from './Auth/Auth';
+import Authenticating from './Auth/Authenticating';
+import Intro from './components/Intro/Intro';
+
+// App Routes
+import Team from './components/Team/Team';
+import Users from './components/Users/Users';
+import SingleUser from './components/Users/SingleUser';
+
+// Create new Auth session
+const auth = new Auth();
+
+const handleAuthentication = (nextState, replace) => {
+  if (/access_token|id_token|error/.test(nextState.location.hash)) {
+    auth.handleAuthentication();
+  }
+}
+
+export const makeMainRoutes = () => {
+  return (
+    <Router history={history} component={App}>
+      <div>
+        <Route path="/" render={(props) => <App auth={auth} {...props} />} />
+        <Route path="/intro" render={(props) => <Intro auth={auth} {...props} />} />
+        <Route path="/authenticating" render={(props) => {
+          handleAuthentication(props);
+          return <Authenticating {...props} /> 
+        }}/>
+        
+        <Route path="/team" render={(props) => <Team auth={auth} {...props} />} />
+        <Route exact path="/users" render={(props) => <Users auth={auth} {...props} />} />
+        <Route exact path="/users/:id" render={(props) => <SingleUser auth={auth} {...props} />} />
+
+      </div>
+    </Router>
+  );
+}
