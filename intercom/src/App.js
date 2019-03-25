79d8d77faf1b39dc0094ad014303aeaae23e746d
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-25 19:19:45 -0400

    Updated App.js to incorporate Auth props and functions.

--- intercom/src/App.js
+++ intercom/src/App.js
@@ -1,35 +1,56 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-
-import Users from './components/Users/Users';
-import SingleUser from './components/Users/SingleUser';
-import Intro from './components/Intro/Intro';
-import Team from './components/Team/Team';
-import Register from './components/Register/Register';
 
 import './App.css';
 
 class App extends Component {
+
+  goTo = () => {
+    this.props.history.replace(`/intro`)
+  }
+
+  login = () => {
+    this.props.auth.login();
+  }
+
+  logout = () => {
+    this.props.auth.logout();
+  }
+
+  componentDidMount = () => {
+    const { renewSession } = this.props.auth;
+
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+      renewSession();
+    }
+  }
+
   render() {
+
+    const { isAuthenticated } = this.props.auth;
+
     return (
-      <Router>
-        <div className="App">
-          <header>
-            <nav>
-              <NavLink to="/team">Team Members </NavLink>
-              <NavLink to="/users">Users </NavLink>
-            </nav>
-          </header>
-          <main>
-            <Route exact path="/" component={Intro} />
-            <Route path="/team" component={Team} />
-            <Route exact path="/users" component={Users} />
-            <Route exact path="/users/:id" component={SingleUser} />
-            <Route path="/register" component={Register} /> 
-          </main>       
-        </div>
-      </Router>
-    );
+
+      <div>
+        <button onClick={this.goTo}>
+          Home
+        </button>
+        {
+          !isAuthenticated() && (
+            <button onClick={this.login}>
+              Log In
+            </button>
+          )
+        }
+        {
+          isAuthenticated() && (
+            <button onClick={this.logout}>
+              Log Out
+            </button>
+          )
+        }
+      </div>
+
+    )
   }
 }
 
