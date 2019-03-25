8ba5680e70b8f4a53be358b957d5656a5e58a580
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-25 17:25:45 -0400

    Refactor App test to confirm component rendering properly when user is authenticated or not.

--- intercom/src/App.test.js
+++ intercom/src/App.test.js
@@ -7,14 +7,37 @@ import App from './App';
 
 describe('<App />', () => {
 
+  const props = {
+    auth: {
+      isAuthenticated: function() {return true}
+    }
+  }
+
   it('renders without crashing', () => {
-    render(<App />);
+    render(<App {...props} />);
   });
 
-  it('confirms team members navlink listed', () => {
-    const { getByText } = render(<App />);
-    const teamMemNavLink = getByText(/Team Members/i);
-    expect(teamMemNavLink).toBeInTheDocument();
+  it('renders home button', () => {
+    const { getByText } = render(<App {...props} />);
+    const homeBtn = getByText(/Home/i);
+    expect(homeBtn).toBeInTheDocument();
+  })
+
+  it('renders logout button if user logged in', () => {
+    const { getByText } = render(<App {...props} />);
+    const logOutBtn = getByText(/Log Out/i);
+    expect(logOutBtn).toBeInTheDocument();
+  })
+
+  it('renders login button if user logged out', () => {
+    const props = {
+      auth: {
+        isAuthenticated: function() {return false}
+      }
+    }
+    const { getByText } = render(<App {...props} />);
+    const logInBtn = getByText(/Log In/i);
+    expect(logInBtn).toBeInTheDocument();
   })
 
 })
