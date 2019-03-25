cf6971b4171cf8d21161311a78ffa007087d72d9
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-25 17:22:40 -0400

    Added variables for authentication to read from .env files based on yarn script.

--- intercom/src/Auth/authVars.js
+++ intercom/src/Auth/authVars.js
@@ -8,4 +8,5 @@ export const AUTH_CONFIG = {
     domain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
     clientID: `${process.env.REACT_APP_AUTH_CLIENT_ID}`,
     redirectUri,
+    audience: `${process.env.REACT_APP_AUTH_AUD}`
 }
