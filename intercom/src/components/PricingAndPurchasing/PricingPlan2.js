9a774182767cddace40bf1228122a8c68786c678
Adrian Adames <adrianadames@gmail.com>
2019-03-28 18:03:15 -0400

    Installed react-stripe-elements library provided by Stripe. Added simple payment form to pricingPlan components.

--- intercom/src/components/PricingAndPurchasing/PricingPlan2.js
+++ intercom/src/components/PricingAndPurchasing/PricingPlan2.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
+import {CardElement, injectStripe} from 'react-stripe-elements';
+
+// The CardElement component imported from react-stripe-elements creates a “card” type element
+// that mounts on the page when the component renders. The CardElement includes inputs for all
+// of the major card fields: the card number, the expiration date, and the CVC. To display 
+// those inputs separately, you can use other Element components provided by the library.
+
 
 class PricingPlan2 extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.submitPaymentHandler = this.submitPaymentHandler.bind(this);
     }
 
+    async submitPaymentHandler(ev) {
+        // User clicked Submit Payment
+
+    }
 
     render() {
         return (
-            <div>
-                PricingPlan2 Card
+            <div style = {{border:'1px solid red'}}>
+                <h2>PricingPlan2 Card</h2>
+                <CardElement />
+                <button onClick = {this.submitPaymentHandler}>Submit Payment</button>
             </div>
         )
     }
 }
 
-export default PricingPlan2;
+export default injectStripe(PricingPlan2);
