47c288a1ab85d7b30850bac1b0a7e853d8affdd4
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2019-03-28 15:21:57 -0700

    groupsBelonged components added to the user component

--- intercom/src/components/User/User.js
+++ intercom/src/components/User/User.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import GroupForm from '../Groups/GroupForm';
+import GroupsBelonged from '../Groups/GroupsBelonged';
 import host from '../../host';
 
 class User extends Component {
     state = {
         user: {},
+        groupsBelongedTo: []
     }
 
     componentWillMount() {
@@ -24,14 +26,33 @@ class User extends Component {
             });
     }
 
+    componentDidMount() {
+        const id = localStorage.getItem('userId')        
+        const groupsBelongedTo = `${host}/api/users/${id}/groupsBelongedTo`;
+        console.log(groupsBelongedTo)
+        axios.get(groupsBelongedTo)
+            .then(res => {
+                this.setState({ groupsBelongedTo: res.data })
+            })
+            .catch(err => {
+                this.setState({
+                    error: err.response.data.message,
+                    groupsBelongedTo: []
+                });
+            });
+    }
+    
+
     render() {
+        console.log(this.state.groupsBelongedTo)
         return (
             <>
                 {this.state.error
                     ? <p>Error retrieving user!</p>
                     : <div>
                         {this.state.user.id} {this.state.user.displayName} {this.state.user.email}
-                        <GroupForm id={this.state.id} />                        
+                        <GroupForm id={this.state.id} /> 
+                        <GroupsBelonged groupsBelonged={this.state.groupsBelongedTo}/>                     
                     </div>
                 }
             </>
