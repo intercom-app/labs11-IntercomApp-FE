b9e4f01df01b9846c6de81859d22b6f03d97897b
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-29 00:23:13 -0400

    Refactor parent and child components for grouppchatroom view to pass props to child.

--- intercom/src/components/GroupChatroom/GroupChatroomCall.js
+++ intercom/src/components/GroupChatroom/GroupChatroomCall.js
@@ -1,68 +1,67 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
-import host from '../../host';
+// import host from '../../host';
 
 class GroupChatroomCall extends Component {
-    state = {
-        group: {},
-        participants: [],
-        phoneNumber: '',
-        error: null,
-    }
+    // state = {
+    //     group: {},
+    //     participants: [],
+    //     phoneNumber: '',
+    //     error: null,
+    // }
 
-    componentDidMount = async () => {
-        const groupById = `${host}/api/groups/${this.props.groupId}`;
-        try {
-            const res = await axios.get(groupById)
-            this.setState({ group: res.data })
-        } catch (err) {
-            this.setState({ error: err.response.data.message })
-        }
+    // componentDidMount = async () => {
+    //     const groupById = `${host}/api/groups/${this.props.groupId}`;
+    //     try {
+    //         const res = await axios.get(groupById)
+    //         this.setState({ group: res.data })
+    //     } catch (err) {
+    //         this.setState({ error: err.response.data.message })
+    //     }
 
-        this.getParticipants()
-        console.log(this.state.group)
+    // this.getParticipants()
 
-    }
+    // }
 
-    getParticipants = async () => {
-        const participants = `${host}/api/groups/${this.props.groupId}/callParticipants`;
-        try {
-            const res = await axios.get(participants)
-            this.setState({ participants: res.data })
-        } catch (err) {
-            this.setState({ error: err.response.data.message })
-        }
-    }
+    // getParticipants = async () => {
+    //     const participants = `${host}/api/groups/${this.props.groupId}/callParticipants`;
+    //     try {
+    //         const res = await axios.get(participants)
+    //         this.setState({ participants: res.data })
+    //     } catch (err) {
+    //         this.setState({ error: err.response.data.message })
+    //     }
+    // }
 
     render() {
 
+        let {callStatus, phoneNumber} = this.props.group;
+        let {participants} = this.props
+
         return (
             <>
-                {this.state.error
-                    ? <p>{this.state.error}</p>
-                    : <>
-                        <button>
-                            {this.state.group.callStatus === 0 ? 'Start Chat' : 'Join Chat'}
-                        </button>
 
-                        <h5>Phone Number</h5>
-                        {this.state.group.phoneNumber
-                            ? this.state.group.phoneNumber
-                            : 'No Active Phone Number'
-                        }
+                <button>
+                    {callStatus === 0 ? 'Start Chat' : 'Join Chat'}
+                </button>
 
-                        <h5>Call Participants</h5>
-                        <ul>
-                            {this.state.participants.map(user =>
-                                <li key={user.userId}>
-                                    {user.displayName}
-                                </li>
-                            )}
-                        </ul>
-                    </>
+                <h5>Phone Number</h5>
+                {phoneNumber
+                    ? phoneNumber
+                    : 'No Active Phone Number'
                 }
+
+                <h5>Call Participants</h5>
+                <ul>
+                    {participants.map(user =>
+                        <li key={user.userId}>
+                            {user.displayName}
+                        </li>
+                    )}
+                </ul>
             </>
+
         )
     }
 
--- intercom/src/components/GroupChatroom/GroupChatroomView.js
+++ intercom/src/components/GroupChatroom/GroupChatroomView.js
@@ -13,25 +13,37 @@ class GroupChatroomView extends Component {
         userId: localStorage.getItem('userId'),
         groupId: this.props.match.params.id,
         group: {},
+        activities: [],
+        participants: [],
         isOwner: false,
         error: null,
     }
 
     componentDidMount = async () => {
-        const groupById = `${host}/api/groups/${this.state.groupId}`;
-        try {
-            const res = await axios.get(groupById)
-            this.setState({ group: res.data })
-        } catch (err) {
-            this.setState({ error: err.response.data.message })
-        }
-
-        this.checkIfOwner();
-
+        const id = this.state.groupId;
+        this.getGroup(id);
+        this.getActivities(id);
+        this.getParticipants(id);
+        this.checkIfOwner(id);
     }
 
-    checkIfOwner = async () => {
-        const groupOwners = `${host}/api/groups/${this.state.groupId}/groupOwners`;
+    getGroup = id => {
+        const groupById = `${host}/api/groups/${id}`;
+        this.axiosGet(groupById, 'group')
+    }
+
+    getActivities = id => {
+        const activities = `${host}/api/groups/${id}/activities`;
+        this.axiosGet(activities, 'activities')
+    }
+
+    getParticipants = id => {
+        const participants = `${host}/api/groups/${id}/callParticipants`;
+        this.axiosGet(participants, 'participants')
+    }
+
+    checkIfOwner = async (id) => {
+        const groupOwners = `${host}/api/groups/${id}/groupOwners`;
         try {
             const res = await axios.get(groupOwners)
             const isOwner = res.data.filter(owner => owner.userId === this.state.userId)
@@ -44,24 +56,36 @@ class GroupChatroomView extends Component {
 
     }
 
+    axiosGet = async (call, key) => {
+        try {
+            const res = await axios.get(call)
+            this.setState({ [key] : res.data })
+        } catch (err) {
+            this.setState({ error: err.response.data.message })
+        }
+    }
+
     render() {
+
+        let { group, groupId, isOwner, participants, error } = this.state
 
         return (
             <>
-                {this.state.error
-                    ? <p>{this.state.error}</p>
+                {error
+                    ? <p>{error}</p>
                     : <>
-                        <h3>{this.state.group.name}</h3>
-                        <Link to={`/group/${this.state.groupId}/members`}>
-                            {this.state.isOwner ? 'Manage Members' : 'View Members'}
+                        <h3>{group.name}</h3>
+                        <Link to={`/group/${groupId}/members`}>
+                            {isOwner ? 'Manage Members' : 'View Members'}
                         </Link>
 
                         <GroupChatroomCall
-                            groupId={this.state.groupId}
+                            group={group}
+                            participants={participants}
                         />
                         
                         <GroupChatroomActivities
-                            groupId={this.state.groupId}
+                            groupId={groupId}
                         />
                     </>
                 }
