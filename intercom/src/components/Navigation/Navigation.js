ab5131187448a4b7b76d225ba527cabea5ab9a54
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-04-02 17:27:17 -0400

    Updated account setting navlink bug to Link to proper account route no more which page user in on.

--- intercom/src/components/Navigation/Navigation.js
+++ intercom/src/components/Navigation/Navigation.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import {
     Collapse,
     Navbar,
@@ -25,16 +24,14 @@ export default class Example extends React.Component {
         return (
             <div>
                 <Navbar color="light" light expand="md">
-                <NavLink href={`/user/${this.props.id}`}>Home</NavLink>
+                    <NavLink href={`/user/${this.props.id}`}>Home</NavLink>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
                             <NavItem>
-                                <Link to={`/user/${this.props.id}/account`}>
-                                    <NavLink>
-                                        Account Settings
-                                    </NavLink>
-                                </Link>
+                                <NavLink href={`/user/${this.props.id}/account`}>
+                                    Account Settings
+                                </NavLink>
                             </NavItem>
                             <NavItem>
                                 <NavLink onClick={this.props.logout}>Logout</NavLink>
