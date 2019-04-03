134060a2876b7db6f617946aeb675d602e8a8e01
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-04-03 14:23:55 -0400

    Further refactoring of groups owned with html template styling.

--- intercom/src/components/Groups/GroupsOwned.js
+++ intercom/src/components/Groups/GroupsOwned.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-// import { NavLink } from "react-router-dom";
-// import { Table } from 'reactstrap';
-// import CallParticipants from './CallParticipants';
-// import CallStatus from './CallStatus';
+
+import CallParticipants from './CallParticipants';
+import CallStatus from './CallStatus';
 
 require('../../styling/css/bootstrap.min.css');
 require('../../styling/css/patros.css');
@@ -12,31 +11,6 @@ class GroupsOwned extends Component {
     render() {
         return (
             <>
-                {/*
-                    <div className='mt-sm-3'>
-                        <h2>Groups Owned</h2>
-                        <Table>
-                            <thead>
-                                <tr>
-                                    <th>id</th>
-                                    <th>Group Name</th>
-                                    <th>Participants</th>
-                                    <th>Call Status</th>
-                                </tr>
-                            </thead>
-                            {this.props.groupsOwned.map((group, key) => (
-                                <tbody key={key}>
-                                    <tr>
-                                        <td><NavLink to={`/group/${group.groupId}`} >{group.groupId}</NavLink></td>
-                                        <td>{group.GroupName} </td>
-                                        <CallParticipants groupId={group.groupId} />
-                                        <CallStatus groupId={group.groupId} />
-                                    </tr>
-                                </tbody>
-                            ))}
-                        </Table>
-                    </div>
-                */}
 
                 <div data-spy="scroll">
 
@@ -46,21 +20,24 @@ class GroupsOwned extends Component {
                             {/* <!-- Blog Column --> */}
                             <div className="col-md-8">
                                 <h1 className="page-header sidebar-title">
-                                    Company Blog
-                            </h1>
-                                {/* <!-- First Blog Post --> */}
-                                <div className="row blogu">
-                                    <div className="col-sm-8 col-md-8">
-                                        <h2 className="blog-title">
-                                            Post title 1
-                                    </h2>
-                                        <p><i className="fa fa-calendar-o"></i>  August 28, 2013
-                                        <span className="comments-padding"></span>
-                                            <i className="fa fa-comment"></i> 0 comments
-                                    </p>
+                                    Groups Owned
+                                </h1>
+
+                                {this.props.groupsOwned.map( group => (
+                                    <div className="row blogu" key={group.groupId}>
+                                        <div className="col-sm-8 col-md-8">
+                                            <h2 className="blog-title">
+                                                {group.GroupName}
+                                            </h2>
+                                            <CallParticipants groupId={group.groupId} />
+                                            <CallStatus groupId={group.groupId} />
+                                        </div>
+                                        <hr></hr>
                                     </div>
-                                </div>
-                                <hr></hr>
+                                    
+                                ))}
+    
+
                             </div>
                         </div>
                     </section>
