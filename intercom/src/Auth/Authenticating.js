c2d494aa09c5573161338ff93931c06dc72cb5ed
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-04-04 19:18:08 -0400

    Updated authenticating screen to have loader.

--- intercom/src/Auth/Authenticating.js
+++ intercom/src/Auth/Authenticating.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import Loader from 'react-loader-spinner'
 
 class Authenticating extends Component {
   render() {
 
     return (
-      <div>
-          Authenticating...
-      </div>
+      <section className="container blog">
+        <h2>Authenticating...</h2>
+        <div style={{ textAlign: "center" }}>
+          <Loader type="Bars" color="#33cc33" height="100" width="100" />
+        </div>
+      </section>
     );
   }
 }
