ceb8246d81bfa6788754015f232024b9f477863a
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-04-03 21:25:15 -0400

    Begin initial styling of search bar.

--- intercom/src/components/Search/SearchBar.js
+++ intercom/src/components/Search/SearchBar.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import { Form, FormGroup, Label, Input } from 'reactstrap';
+// import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 const SearchBar = (props) => {
     return (
-        <Form>
+        <>
+            <aside className="col-md-4 sidebar-padding">
+                <div className="blog-sidebar">
+                    <h3 className="sidebar-title">Invite New Members</h3>
+                    <hr></hr>
+                    <h4 className="sidebar-title">Search Users: </h4>
+                    <div className="input-group">
+                        <input
+                            className="form-control"
+                            type="text"
+                            name="searchUser"
+                            id="searchUser"
+                            placeholder="Search for user..."
+                            value={props.inputValue}
+                            onChange={props.updateSearch}
+                        />
+                    </div>
+                </div>
+            </aside>
+            {/* <Form>
             <FormGroup>
                 <Label for="searchUser">Invite new members</Label>
                 <Input
@@ -15,8 +34,8 @@ const SearchBar = (props) => {
                     onChange={props.updateSearch}
                 />
             </FormGroup>
-        </Form>
-
+        </Form> */}
+        </>
     );
 }
 
