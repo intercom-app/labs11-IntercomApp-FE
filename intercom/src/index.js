5529c672b7793aeccdf7a5af5cc16a484833a736
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-04-04 15:15:10 -0400

    Refactor app to properly render html template and utilize scripts.

--- intercom/src/index.js
+++ intercom/src/index.js
@@ -1,13 +1,4 @@
 import ReactDOM from 'react-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './index.css';
-import './styling/css/bootstrap.min.css'
-import './styling/css/patros.css'
-import './styling/font-awesome/css/font-awesome.min.css'
-
-// import './styling/js/jquery.js'
-// import './styling/js/jquery.quicksand.js'
-// import'./styling/js/bootstrap.min.js'
 
 import { makeMainRoutes } from './routes';
 
