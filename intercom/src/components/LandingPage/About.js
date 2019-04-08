b5adce81a64e4fe48dbaa91fd03c83c2e8a0ea94
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-04-08 18:39:51 -0400

    Styling of services/features component of landing page.

--- intercom/src/components/LandingPage/About.js
+++ intercom/src/components/LandingPage/About.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
 const About = () => {
-    return (
+	return (
 		<section id="about">
 			<div className="container">
 				<div className="row">
 					<div className="col-md-offset-1 col-md-10">
 						<div className="text-center">
 							<h2>Mission Statement</h2>
-							<img className="img-responsive displayed" src="images/short.png" alt="Company about"/>
+							<img className="img-responsive displayed" src="images/short.png" alt="Company about" />
 							<div className="row">
 								<div className="col-md-12">
 									<p>
-                                    Our mission is to assist a wide variety of users ranging from those who may be hearing impaired or elderly to anyone who finds themselves as a group having difficulty conversing with others with overbearing background noise. 
-                                    We provide our users to opportunity to easily create and join personal groups and initiate voice chats within those groups to keep a clear communication link with their family and friends. 
-                                    </p>						
+										Our mission is to assist a wide variety of users ranging from those who may be hearing impaired or elderly to anyone who finds themselves as a group having difficulty conversing with others with overbearing background noise.
+										We provide our users to opportunity to easily create and join personal groups and initiate voice chats within those groups to keep a clear communication link with their family and friends.
+                  </p>
 								</div>
 							</div>
 						</div>
@@ -22,7 +22,7 @@ const About = () => {
 				</div>
 			</div>
 		</section>
-    );
+	);
 }
 
 export default About;
--- intercom/src/components/LandingPage/Services.js
+++ intercom/src/components/LandingPage/Services.js
@@ -2,10 +2,57 @@ import React from 'react';
 
 const Services = () => {
     return (
-        <>
-            
-        </>
-
+		<section id="services">
+			<div className="greenback">
+				<div className="container">
+					<div className="text-center homeport2"><h2>Features</h2></div>
+					<div className="row">
+						<div className="col-md-12 homeservices1">
+							<div className="col-md-3 portfolio-item">
+								<div className="text-center">
+									<span className="fa-stack fa-lg">
+									  <i className="fa fa-circle fa-stack-2x"></i>
+									  <i className="fa fa-sign-in fa-stack-1x"></i>
+									</span>
+									<h3>Trusted Sign-Up</h3>
+									<p>Registration and Login is handled entirely by Auth0, a secure and trusted authentication platform. Simply use your gmail account or another preferred email and password to begin.</p>
+								</div>
+							</div>
+							<div className="col-md-3 portfolio-item">
+								<div className="text-center">									
+									<span className="fa-stack fa-lg">
+									  <i className="fa fa-circle fa-stack-2x"></i>
+									  <i className="fa fa-users fa-stack-1x"></i>
+									</span>
+									<h3>Group Chatrooms</h3>
+									<p>Create personal group chatrooms and invite your family and friends to join. Once group members have accepted their invitations you can start communicating with ease.</p>
+								</div>
+							</div>
+							<div className="col-md-3 portfolio-item">
+								<div className="text-center">
+									<span className="fa-stack fa-lg">
+									  <i className="fa fa-circle fa-stack-2x"></i>
+									  <i className="fa fa-phone fa-stack-1x"></i>
+									</span>
+									<h3>Chatroom Calls</h3>
+									<p>Conversations can happen anywhere at anytime. Using Twilio, a reliable voice application, Voice Chatroom allows you the ability to start and join real-time conversations within your group.</p>
+								</div>
+							</div>
+							<div className="col-md-3 portfolio-item">
+								<div className="text-center">
+									<span className="fa-stack fa-lg">
+									  <i className="fa fa-circle fa-stack-2x"></i>
+									  <i className="fa fa-cogs fa-stack-1x"></i>
+									</span>
+									<h3>Secure Billing</h3>
+									<p>Your online payments are safe with us. We utilize Stripe, the best billing software platform, to securely handle all online payments and billing options for our users.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
     );
 }
 
