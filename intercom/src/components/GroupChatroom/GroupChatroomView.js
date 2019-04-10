598f5cd10473a1afe1e921a9cf2f8375732d2efb
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-04-10 16:34:56 -0400

    removed unneeded import from chatroom view  component.

--- intercom/src/components/GroupChatroom/GroupChatroomView.js
+++ intercom/src/components/GroupChatroom/GroupChatroomView.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'reactstrap';
 import axios from 'axios';
 import host from '../../host';
 import GroupChatroomActivities from './GroupChatroomActivities';
