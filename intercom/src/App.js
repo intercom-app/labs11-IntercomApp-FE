56c9c79ae66bac28faaf6f9fbf92ccd861efe8a0
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-04-16 16:10:20 -0400

    Added jquery functions to app on CDM and CDU to account for nav links and scrolling.

--- intercom/src/App.js
+++ intercom/src/App.js
@@ -4,14 +4,6 @@ import Navigation from './components/Navigation/Navigation';
 
 class App extends Component {
 
-  login = () => {
-    this.props.auth.login();
-  }
-
-  logout = () => {
-    this.props.auth.logout();
-  }
-
   componentDidMount = () => {
     const { renewSession } = this.props.auth;
 
@@ -19,15 +11,78 @@ class App extends Component {
       renewSession();
     }
 
+    this.scrollToTop();
+    this.scrollSmooth();
+  }
+
+  componentDidUpdate = () => {
+    this.scrollToTop();
+    this.scrollSmooth();
+  }
+  
+  scrollToTop = () => {
     window.$(document).ready(function(){
       window.$(this).scrollTop(0);
     });
   }
 
-  componentDidUpdate = () => {
-    window.$(document).ready(function(){
-      window.$(this).scrollTop(0);
+  scrollSmooth = () => {
+    window.$(document).ready(function () {
+
+      window.$('.custom-menu a[href^="#"], .intro-scroller .inner-link').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = window.$(target);
+
+        window.$('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+          window.location.hash = target;
+        });
+      });
+
+      window.$('a.page-scroll').bind('click', function (event) {
+        var $anchor = window.$(this);
+        window.$('html, body').stop().animate({
+          scrollTop: window.$($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+      });
+
+      window.$(".nav a").on("click", function () {
+        window.$(".nav").find(".active").removeClass("active");
+        window.$(this).parent().addClass("active");
+      });
+
+      window.$('body').append('<div id="toTop" class="btn btn-primary color1"><span class="glyphicon glyphicon-chevron-up"></span></div>');
+      window.$(window).scroll(function () {
+        if (window.$(this).scrollTop() !== 0) {
+          window.$('#toTop').fadeIn();
+        } else {
+          window.$('#toTop').fadeOut();
+        }
+      });
+      window.$('#toTop').click(function () {
+        window.$("html, body").animate({ scrollTop: 0 }, 700);
+        return false;
+      });
+
     });
+
+  }
+
+  login = () => {
+    this.props.auth.login();
+  }
+
+  logout = () => {
+    this.props.auth.logout();
+    window.$(document).ready(function(){
+      window.$(".nav").find(".active").removeClass("active");
+      window.$(".nav a:first").parent().addClass("active");
+    });
+
   }
 
   render() {
