e9956bdba328d90776e2c49f49f29e612f86f131
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-04-17 18:34:57 -0400

    Updating styling of unauthorized page to fit all media views.

--- intercom/src/components/User/UnAuth.js
+++ intercom/src/components/User/UnAuth.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
-
 class UnAuth extends Component {
 
     render() { 
-        // const img = require('../../images/avatar1.png')
         const id = localStorage.getItem('userId')
         return ( 
-            <section className="container blog">
-                <div style={{ textAlign: "center", marginBottom: "5%" }}>
-                    <h2 >You shall not pass!</h2>
-                    <Link to={`/user/${id}/`} className=''>Go back to your profile</Link>
+            <section className='unauth'>
+                <div className='unauth-body'>
+                    <h1 className='unauth-header'>Access Denied</h1>
+                    <h2 className='unauth-header-sub'>You are not authorized to access this page.</h2>
+                    <h2 className='unauth-header-sub'><Link to={`/user/${id}/`} className=''>Go back to your profile</Link></h2>
                 </div>
-                <div className='unauth'></div>
+                <div className='unauth-img'></div>
             </section>
         );
     }
